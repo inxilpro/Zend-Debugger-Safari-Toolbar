d1216145373541c16f9c0d391a30d16dd96ba054
Chris Morrell <github@rchy.net>
2010-07-01 17:52:07 -0400

    Removed debugging message and set it to start debugging session without the need to reload.

--- scripts/proxy.js
+++ scripts/proxy.js
@@ -2,11 +2,15 @@ safari.self.addEventListener("message", function(event) {
 	var command = event.name;
 	var data = event.message;
 	
-	alert('Running: ' + command);
+	// alert('Running: ' + command);
 	if (undefined !== data.cookies) {
 		for (c in data.cookies) {
 			console.log('Set "' + c + '" to "' + data.cookies[c] + '".');
 			document.cookie = c + '=' + escape(data.cookies[c]) + '; expires=Mon, 12 Feb 2035 01:00:00 UTC; path=/';
 		}
+	}
+	
+	if (true == data.reload) {
+		window.location.reload();
 	}
 }, false);
